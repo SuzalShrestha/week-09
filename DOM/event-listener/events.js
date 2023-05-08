63975bb0e2d290fc07ee01317a5bff5420c38948
Sujal Shrestha <sujal.scxi121@amv.edu.np>
2023-05-08 07:53:28 +0545

    completed add item function in event-listener

--- DOM/event-listener/events.js
+++ DOM/event-listener/events.js
@@ -1,10 +1,23 @@
 // Your code here
 window.addEventListener("DOMContentLoaded", () => {
-  alert("DOM fully loaded and parsed");
+  console.log("DOM fully loaded and parsed");
   const redButton = document.getElementById("red-input");
   redButton.addEventListener("input", (e) => {
     if (e.target.value === "red") {
       redButton.style.backgroundColor = "red";
     }
+  });
+
+  const input = document.getElementById("list-add");
+  let text = "";
+  input.addEventListener("change", (e) => {
+    text = e.target.value;
+  });
+  const button = document.getElementById("add-item");
+  button.addEventListener("click", () => {
+    const list = document.querySelector("ul");
+    const li = document.createElement("li");
+    li.textContent = text;
+    list.appendChild(li);
   });
 });
